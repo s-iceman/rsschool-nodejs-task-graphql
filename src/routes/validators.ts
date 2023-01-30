83f37e3b4570f7616a3b4c553c7167281802790d
s_iceman <s_iceman@protonmail.com>
2023-01-30 12:45:48 +0300

    refactor: remove debug function

--- src/routes/validators.ts
+++ src/routes/validators.ts
@@ -1,6 +1,3 @@
-import * as alert from 'alert';
-
-
 const uuidTemplate = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
 
 
@@ -8,13 +5,6 @@ function validateUuid(str: string): boolean {
   return uuidTemplate.test(str);
 }
 
-function myDebug(data: any): void {
-   setTimeout(() => { 
-    alert(JSON.stringify(data, null, '\n'));
-    }, 500);
-}
-
 export {
-  validateUuid,
-  myDebug
+  validateUuid
 }
